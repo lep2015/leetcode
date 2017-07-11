9dd89ef6162ac39be716ea829fabe0cc9454e925
lack <lep2014@163.com>
2017-07-11 21:14:32 +0800

    add merge k list

---
+++ src/org/lep/leetcode/mergeklist/MergeKList.java
@@ -0,0 +1,131 @@
+package org.lep.leetcode.mergeklist;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Source : https://oj.leetcode.com/problems/merge-k-sorted-lists/
+ *
+ * Created by lverpeng on 2017/7/11.
+ *
+ * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+ *
+ */
+public class MergeKList {
+
+    /**
+     * 两两归并排序
+     *
+     * @param headList
+     * @return
+     */
+    public Node merge (List<Node> headList) {
+        while (headList.size() > 1) {
+            Node list1 = headList.remove(headList.size() - 1);
+            Node list2 = headList.remove(headList.size() - 1);
+            Node mergeList = mergeTwoList(list1, list2);
+            headList.add(0, mergeList);
+        }
+        return headList.get(0);
+
+    }
+
+
+    /**
+     * 归并排序
+     *
+     * @param list1
+     * @param list2
+     * @return
+     */
+    public static Node mergeTwoList (Node list1, Node list2) {
+        Node head = null;
+        Node current = null;
+        while (list1 != null && list2 != null) {
+            Node node = null;
+            if (list1.value > list2.value) {
+                node = list2;
+                list2 = list2.next;
+            } else {
+                node = list1;
+                list1 = list1.next;
+            }
+            if (head == null) {
+                head = current = node;
+            } else {
+                current.next = node;
+                current = current.next;
+            }
+        }
+
+        list1 = list1 == null ? list2 : list1;
+        if (list1 != null) {
+            if (head != null) {
+                current.next = list1;
+            } else {
+                head = list1;
+            }
+        }
+        return head;
+    }
+
+
+
+
+    private static class Node {
+        int value;
+        Node next;
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value=" + value +
+                    ", next=" + (next == null ? "" : next.value) +
+                    '}';
+        }
+    }
+
+    private static void print (Node node) {
+        while (node != null) {
+            System.out.println(node);
+            node = node.next;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Node list1 = new Node();
+        Node pointer1 = list1;
+        list1.value = 1;
+        Node list2 = new Node();
+        list2.value = 2;
+        Node pointer2 = list2;
+        Node list3 = new Node();
+        list2.value = 3;
+        Node pointer3 = list3;
+        for (int i = 4; i < 20; i++) {
+            Node node = new Node();
+            node.value = i;
+            if (i % 3 == 1) {
+                pointer1.next = node;
+                pointer1 = pointer1.next;
+            } else if (i % 3 == 2) {
+                pointer2.next = node;
+                pointer2 = pointer2.next;
+            } else {
+                pointer3.next = node;
+                pointer3 = pointer3.next;
+            }
+        }
+
+        List<Node> list = new ArrayList<Node>();
+        list.add(list1);
+        list.add(list2);
+        list.add(list3);
+        MergeKList mergeKList = new MergeKList();
+        Node result = mergeKList.merge(list);
+        print(result);
+
+    }
+}
