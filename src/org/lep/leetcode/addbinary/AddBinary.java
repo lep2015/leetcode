39aa960d1a40852a57e7b78a61e34f4a193fbe8c
lack <lep2014@163.com>
2017-07-22 17:11:55 +0800

    add binary

--- src/org/lep/leetcode/addbinary/AddBinary.java
+++ src/org/lep/leetcode/addbinary/AddBinary.java
@@ -14,4 +14,38 @@ package org.lep.leetcode.addbinary;
  */
 public class AddBinary {
 
+    public String add (String binary1, String binary2) {
+        StringBuilder result = new StringBuilder();
+        int length = binary1.length() > binary2.length() ? binary2.length() : binary1.length();
+        int carry = 0;
+        for (int i = length - 1; i > -1; i--) {
+            int sum = Integer.parseInt(binary1.substring(i, i+1)) + Integer.parseInt(binary2.substring(i, i+1)) + carry;
+            carry = sum / 2;
+            result.insert(0, sum % 2);
+        }
+        String longerStr = "";
+        if (binary1.length() > binary2.length()) {
+            longerStr = binary1;
+        } else {
+            longerStr = binary2;
+        }
+        for (int i = longerStr.length() - 1; i >= length; i--) {
+            int sum = Integer.parseInt(longerStr.substring(i, i+1)) + carry;
+            carry = sum / 2;
+            result.insert(0, sum % 2);
+
+        }
+        if (carry > 0) {
+            result.insert(0, carry);
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        AddBinary addBinary = new AddBinary();
+        System.out.println(addBinary.add("", ""));
+        System.out.println(addBinary.add("1", ""));
+        System.out.println(addBinary.add("1", "1"));
+        System.out.println(addBinary.add("10", "11"));
+    }
 }
